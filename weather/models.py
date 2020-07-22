d873c621da1be37117cd4367b90b7cece7a45462
Maureen435 <mchepkemoi435@gmail.com>
2020-07-22 10:54:25 +0300

    modified models

---
+++ weather/models.py
@@ -0,0 +1,14 @@
+from . import db
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer,primary_key = True)
+    city = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'City{self.city}'
+
+#class Country(db.Model):
+#   __table__ = 'country'
+#  id = db.Column(db.Integer, primary_key = True)
+# city = db.Column(db.String(50)) 
