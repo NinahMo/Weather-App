e079093314e75f0576675ca94b5bcc97dfcb0246
peterkush <pkungu004@gmail.com>
2020-07-22 11:35:21 +0300

    modified the sign and registration form.py

--- forms.py
+++ forms.py
@@ -3,3 +3,34 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from models import city
+
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise validationError('The username name is taken.Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email is taken.Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+      
+
