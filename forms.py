55dbe9604007575c3a05eed2c29638a84ff1ba53
peterkush <pkungu004@gmail.com>
2020-07-22 11:37:13 +0300

    created the update account class at file forms.py

--- forms.py
+++ forms.py
@@ -32,5 +32,22 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-      
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise validationError('The username name is taken.Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email is taken.Please choose a different one.')
+            
 
