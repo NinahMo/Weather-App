3a96ec9afac750d2514d7e21fad496a1c995ae12
Ashley Mosoti <ninahb746@gmail.com>
2020-07-22 16:45:28 +0300

    Add a route for the about page

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from weather import weather
+# from weather import weather
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def weather():
-    return render_template('index.html')
+    return render_template('weather.html')
 
 # @app.route("index")
 # def index():
