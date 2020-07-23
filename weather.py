0a149dabb6908112416fedd9a2e8693f2352dacf
Ashley Mosoti <ninahb746@gmail.com>
2020-07-24 02:47:02 +0300

    Delete navbar html file

--- weather.py
+++ weather.py
@@ -8,9 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
